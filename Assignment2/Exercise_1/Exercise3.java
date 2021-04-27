e7f3f319729b25dafed8663e557730ef46acda0a
Tontso <tommysketo977@gmail.com>
2021-04-27 09:46:17 +0300

    end

--- Assignment2/Exercise_1/Exercise3.java
+++ Assignment2/Exercise_1/Exercise3.java
@@ -13,7 +13,7 @@ public class Exercise3 {
         fileRtree = new Scanner(new File("Rtree.txt"));
         fileNNqueries = new Scanner(new File("NNqueries.txt"));
         RTree tree = new RTree();
-        int k = 3;
+        int k = 21;
         Double[] query;
         int queryCount = 0;
 
