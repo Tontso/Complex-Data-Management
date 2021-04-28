93adf212e3cb3b0cc95e7281398e6bb2cc809e0d
Tontso <tommysketo977@gmail.com>
2021-04-28 07:23:15 +0300

    filinsh

--- Assignment2/Exercise_1/RTree.java
+++ Assignment2/Exercise_1/RTree.java
@@ -20,19 +20,6 @@ public class RTree {
     private int minSize;
     private static List<Integer> okQuery = new ArrayList<>();
     
-    class CustomComparator implements Comparator<Node>{
-
-        @Override
-        public int compare(Node o1, Node o2) {
-            if(o1.getDistance() > o2.getDistance())
-                return 1;
-            else if(o1.getDistance() < o2.getDistance())
-                return -1;
-            else
-                return 0;
-        }
-       
-    }
 
     class CustomComparatorNeightbor implements Comparator<Double[]>{
 
@@ -93,27 +80,27 @@ public class RTree {
     }
 
     
-    public void constructRTree(int key) {
-        this.checkForLimits(key);
-        tree.put(key+1,new ArrayList<Node>());
+    public void constructRTree(int level) {
+        this.checkForLimits(level);
+        tree.put(level+1,new ArrayList<Node>());
         currentNode = new Node(1);
-        for (Node node : tree.get(key)) {
+        for (Node node : tree.get(level)) {
             if(currentNode == null)
                 currentNode = new Node(1);
-            currentNode.getData().put(node.getId(), node.findNodeMBR(key));
+            currentNode.getData().put(node.getId(), node.findNodeMBR());
             if(currentNode.getData().size() == M){
-                tree.get(key+1).add(currentNode);
+                tree.get(level+1).add(currentNode);
                 currentNode = null;
             }
         }
         if(currentNode != null)
             if(currentNode.getData().size() != 0)
-                tree.get(key+1).add(currentNode);
+                tree.get(level+1).add(currentNode);
         
-        if(tree.get(key+1).size() == 1)
+        if(tree.get(level+1).size() == 1)
             return;
         else
-            this.constructRTree(key+1); 
+            this.constructRTree(level+1); 
     }
 
     public void printTree(){
@@ -221,17 +208,17 @@ public class RTree {
     public String bestFirstknn(Double[] query, RTree tree, Node root, int k) {
         PriorityQueue<Double[]> pq = new PriorityQueue<>(new CustomComparatorNeightbor());
         Node currentNode;
-        double distance = Double.POSITIVE_INFINITY;
+        double distance;
         int count = 0;
         String returnString = "";
-
+        Double[] mbr = new Double[4];
 
  
         for(Integer key : root.getData().keySet()){
             currentNode = tree.getListTree().get(key);
-            currentNode.setDistance(root.getData().get(key), query);
-            distance = currentNode.getDistance();
-            pq.add(new Double[]{distance, (double)key, (double)currentNode.getIsnonleaf()});          
+            mbr = root.getData().get(key);
+            distance = distanceBoxParticle2D(query[0], query[1], mbr[0],mbr[2], mbr[1], mbr[3]);
+            pq.add(new Double[]{distance, (double)key, (double)currentNode.getIsnonleaf(), mbr[0],mbr[2], mbr[1], mbr[3]});          
         }
         
         while(!pq.isEmpty() && count != k){
@@ -239,16 +226,16 @@ public class RTree {
             if(head[2] == 1.0){ //Has NODES
                 currentNode = tree.getListTree().get(head[1].intValue());
                 for(Integer key : currentNode.getData().keySet()){
-                    tree.getListTree().get(key).setDistance(currentNode.getData().get(key), query); //Calculate distance
-                    distance = tree.getListTree().get(key).getDistance();
-                    pq.add(new Double[]{distance, (double)key, (double)tree.getListTree().get(key).getIsnonleaf()});         
+                    mbr = currentNode.getData().get(key);
+                    distance = distanceBoxParticle2D(query[0], query[1], mbr[0],mbr[2], mbr[1], mbr[3]);
+                    pq.add(new Double[]{distance, (double)key, (double)tree.getListTree().get(key).getIsnonleaf(), mbr[0],mbr[2], mbr[1], mbr[3]});         
                 }
             }else if(head[2] == 0.0){
                 currentNode = tree.getListTree().get(head[1].intValue());
                 for(Integer key : currentNode.getData().keySet()){
-                    currentNode.setDistance(currentNode.getData().get(key), query);
-                    distance = currentNode.getDistance();
-                    pq.add(new Double[]{distance, (double)key, 2.0});  // Is polygon  
+                    mbr = currentNode.getData().get(key);
+                    distance = distanceBoxParticle2D(query[0], query[1], mbr[0],mbr[2], mbr[1], mbr[3]);
+                    pq.add(new Double[]{distance, (double)key, 2.0, mbr[0],mbr[2], mbr[1], mbr[3]});  // Is polygon  
                 }
             }else{
                 count++;
@@ -258,5 +245,26 @@ public class RTree {
         }
         return returnString.substring(0, returnString.length()-1);
     }
-       
+
+    
+    public static double distanceBoxParticle2D(double x, double y, double x_min, double y_min, double x_max, double y_max){
+        if (x < x_min) {
+            if (y <  y_min) return HYPOT(x_min-x, y_min-y);
+            if (y <= y_max) return x_min - x;
+                            return HYPOT(x_min-x, y_max-y);
+        } else if (x <= x_max) {
+            if (y <  y_min) return y_min - y;
+            if (y <= y_max) return 0;
+                            return y - y_max;
+        } else {
+            if (y <  y_min) return HYPOT(x_max-x, y_min-y);
+            if (y <= y_max) return x - x_max;
+                            return HYPOT(x_max-x, y_max-y);
+        }
+    }
+
+
+    private static double HYPOT(double x, double y) {
+        return Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
+    }
 }
